e615c692411d45b400bbb8f43a10dfcceeb4ee4c
david.orive@opendeusto.es <david.orive@opendeusto.es@9aa124b0-7f85-95f2-32f4-e1b9b7fe5357>
2012-02-09 18:05:22 +0000

    He añadido un gestor de ventanas y la ventana principal.
    Si alguien encuentra alguna forma de que la ventana principal tenga un aspecto más agradable a la vista que la cambie.

---
+++ trunk/Proyecto_ProgramacionIV/src/bomberman/ventanas/PanelFondo.java
@@ -0,0 +1,59 @@
+package bomberman.ventanas;
+
+import java.awt.*;
+import java.net.*;
+import javax.swing.*;
+
+/**
+ * Este JPanel modificado tiene como objetivo pintar una imagen en el fondo del panel.
+ * @author David
+ * @version 1.0
+ */
+public class PanelFondo extends JPanel{
+	
+	private static final long serialVersionUID = -7461719037402108362L;
+	private ImageIcon imagen;
+	private URL directorio;
+	private Container container;
+	
+	/**
+	 * Constructor principal de la clase PanelFondo.
+	 * @param path - String. Path donde está la imagen.
+	 * @param con - Container. En que container hay que visualizar la imagen,
+	 * nos servirá para saber que medidas tiene y así redimensionar la imagen en todo momento.
+	 */
+	public PanelFondo(String path, Container con)
+	{
+		directorio = getClass().getResource(path);
+		imagen = new ImageIcon(directorio);
+		container = con;
+	}
+	
+	/**
+	 * Método que sobreescribe el método paintComponent de la clase JPanel.
+	 * @param g - Graphics. Para dibujar los componentes.
+	 */
+	public void paintComponent(Graphics g)
+	{
+		if (g instanceof Graphics2D) {
+			Graphics2D g2 = (Graphics2D) g; // El Graphics realmente es Graphics2D
+			//Esto nos ayuda a que la imagen se redimensione correctamente sin perder calidad.
+			g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			g2.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+			
+		}
+		//Le indicamos que dibuje la imagen en la ventana.
+		g.drawImage(imagen.getImage(), 0, 0, container.getWidth(), container.getHeight(), null);
+		/*
+		 * Le indicamos al panel que no dibuje su fondo por defecto, porque sino nos taparía
+		 * nuestra imagen.
+		 */
+		setOpaque(false);
+		//Le indicamos que pinte el resto de los componentes.
+		super.paintComponent(g);
+		
+	}
+}
+
+
---
+++ trunk/Proyecto_ProgramacionIV/src/bomberman/ventanas/VentanaInicial.java
@@ -0,0 +1,180 @@
+package bomberman.ventanas;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.UIManager;
+
+/**
+ * Ventana inicial del juego que nos ofrece varias opciones.
+ * Extiende de JFrame.
+ * Implementa el interfaz ActionListener.
+ * @author David
+ * @version 1.0
+ */
+public class VentanaInicial extends JFrame implements ActionListener{
+
+	private static final long serialVersionUID = -7461719037402108362L;
+	private JLabel jlTitulo;
+	private JButton jbBotonJugar;
+	private JButton jbBotonPuntu;
+	private JButton jbBotonInstruc;
+	private JButton jbBotonSobre;
+	private String strPath;
+	private PanelFondo pfFondo;
+	
+	/**
+	 * Constructor principal, crea una ventana Inicial.
+	 */
+	public VentanaInicial()
+	{
+		strPath = "bomberman.jpg";
+		jlTitulo = new JLabel("Bomberman");
+		jbBotonJugar = new JButton("JUGAR");
+		jbBotonPuntu = new JButton("SCORES");
+		jbBotonInstruc = new JButton("INSTRUCCIONES");
+		jbBotonSobre = new JButton("SOBRE...");
+		
+		/*
+		 * Creamos un objeto de tipo PanelpfFondo que hará que se vea una imagen en el pfFondo de la pantalla.
+		 */
+		pfFondo = new PanelFondo(strPath, this);
+		
+		/*
+		 * Para que todos los componentes esten bien centrados determinamos su alineamiento en el eje X.
+		 */
+		jlTitulo.setAlignmentX(Component.CENTER_ALIGNMENT);
+		jbBotonJugar.setAlignmentX(Component.CENTER_ALIGNMENT);
+		jbBotonPuntu.setAlignmentX(Component.CENTER_ALIGNMENT);
+		jbBotonInstruc.setAlignmentX(Component.CENTER_ALIGNMENT);
+		jbBotonSobre.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		/*
+		 * Modificamos las características del título para que tenga un buen aspecto.
+		 */
+		jlTitulo.setFont(new Font("sansserif", Font.BOLD, 50));
+		jlTitulo.setForeground(Color.red);
+		
+		//Layout
+		pfFondo.setLayout(new BoxLayout(pfFondo, BoxLayout.Y_AXIS));
+		
+		//Metemos los componentes en el contenedor.
+		meterComponente(jlTitulo);
+		meterComponente(jbBotonJugar);
+		meterComponente(jbBotonPuntu);
+		meterComponente(jbBotonInstruc);
+		meterComponente(jbBotonSobre);
+		
+		/*
+		 * A cada botón le añadimos un escuchador, al ser 'addActionListener' le añadirá el
+		 * escuchador más típico, la propia ventana.
+		 */
+		jbBotonJugar.addActionListener(this);
+		jbBotonPuntu.addActionListener(this);
+		jbBotonInstruc.addActionListener(this);
+		jbBotonSobre.addActionListener(this);
+		
+		//Añadimos el JPanel pfFondo al contenedor principal.
+		getContentPane().add(pfFondo);
+		
+		/*
+		 * Determinamos los valores principales de la ventana.
+		 */
+		this.setSize(440, 598);
+		this.setTitle("Menú Principal");
+		/*
+		 * Determinamos un tamaño mínimo de la ventana aunque dejamos que
+		 * tenga un tamaño máximo. De esta manera puede seguir funcionando
+		 * a la perfección en distintas resoluciones.
+		 */
+		this.setMinimumSize(new Dimension (350, 300));
+		this.setLocationRelativeTo(null);
+		this.setVisible(false);
+		
+		/*
+		 * Queremos que cuando la ventana se cierre el programa entero termine su ejecución.
+		 */
+		this.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				/*
+				 * En caso de que escuche un evento para cerrar la ventana
+				 * le diremos que acabe la ejecución del programa entero.
+				 */
+				System.exit(0);
+			}
+		});
+		
+	}
+	
+	/**
+	 * Implementamos el método 'actionPerformed' del interface ActionListener.
+	 * @param e - ActionEvent. Que evento ha tenido lugar.
+	 */
+	public void actionPerformed(ActionEvent e)
+	{
+		/*
+		 * Para saber dónde se originó el evento creamos un Object con la dirección del
+		 * generador del evento.
+		 */
+		Object botonpulsado = e.getSource();
+		
+		//En caso de que el botón pulsado sea 'jbBotonJugar'.
+//		if(botonpulsado == jbBotonJugar)
+//		{
+//			//Hacemos visible la ventana 'VentanaDatos'.
+//			GestorVentana.hacerVisible(VentanaDatos.class, false);
+//		}
+//		
+//		//En caso de que el botón pulsado sea 'jbBotonPuntu'.
+//		if(botonpulsado == jbBotonPuntu)
+//		{
+//			//Hacemos visible la ventana 'VentanaScore'.
+//			GestorVentana.hacerVisible(VentanaScore.class, false);
+//		}
+//		
+//		//En caso de que el botón pulsado sea 'jbBotonInstruc'.
+//		if(botonpulsado == jbBotonInstruc)
+//		{
+//			//Hacemos visible la ventana 'VentanaInstrucciones'.
+//			GestorVentana.hacerVisible(VentanaInstrucciones.class, false);
+//		}
+		
+		//En caso de que el botón pulsado sea 'jbBotonSobre'.
+//		if(botonpulsado == jbBotonSobre)
+//		{
+//			//Hacemos visible la ventana 'VentanaInformacion'.
+//			GestorVentana.hacerVisible(VentanaInformacion.class, false);
+//		}
+	}
+	
+	/**
+	 * Con este método logramos meter los componentes de una forma ordenada y centrada.
+	 */
+	public void meterComponente(Component comp)
+	{
+		JPanel tempPanel = new JPanel();
+		//Queremos que este alineado hacia el centro.
+		tempPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		tempPanel.add(comp);
+		//En caso de no poner este atributo no dibujaría la imagen de pfFondo.
+		tempPanel.setOpaque(false);
+		pfFondo.add(tempPanel);
+	}
+	
+	/**
+	 * Este es el método main que nos sirve para comprobar que
+	 * la clase funciona correctamente.
+	 * @param args No utilizados
+	 */
+	public static void main (String [] args)
+	{
+		try {
+			UIManager.setLookAndFeel( "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel" );
+		} catch (Exception e) {
+		}
+		VentanaInicial ventana = new VentanaInicial();
+		ventana.setVisible(true);
+	}
+}
+
+
