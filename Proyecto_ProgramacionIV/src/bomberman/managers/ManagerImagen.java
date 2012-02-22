4590877ef995bb1cd1c54dbdaab76d8c73f51f96
david.orive <david.orive@opendeusto.es>
2012-02-22 19:32:09 +0000

    Añadido HashMap a la clase ManagerImagen y además algún que otro retoque. David

--- Proyecto_ProgramacionIV/src/bomberman/managers/ManagerImagen.java
+++ Proyecto_ProgramacionIV/src/bomberman/managers/ManagerImagen.java
@@ -1,8 +1,10 @@
 package bomberman.managers;
 
+import bomberman.protagonistas.*;
 import java.awt.image.*;
 import java.io.IOException;
 import java.net.URL;
+import java.util.*;
 
 import javax.imageio.ImageIO;
 
@@ -10,13 +12,31 @@ public class ManagerImagen {
 
 	private static URL miUrl;
 	private static BufferedImage biImagen;
+	private static HashMap<String, BufferedImage> sprites = init();
 
+	private static HashMap<String, BufferedImage> init()
+	{
+		return new HashMap<String, BufferedImage>();
+	}
+	
 	public static BufferedImage cargarImagen(String path) {
 		miUrl = ManagerImagen.class.getResource(path);
 		try {
 			biImagen = ImageIO.read(miUrl);
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return biImagen;
+	}
+	
+	public static BufferedImage getImagen(String path)
+	{
+		BufferedImage tempImag = (BufferedImage)sprites.get(path);
+		if(tempImag == null)
+		{
+			tempImag = cargarImagen(path);
+			sprites.put(path, tempImag);
+		}
+		return tempImag;
 	}
 }
