86d79b8386806574c4f35d45f3eaa0b743732157
david.orive <david.orive@opendeusto.es>
2012-04-03 15:01:57 +0000

    Ya se ve el juego.

--- Proyecto_ProgramacionIV/src/bomberman/ventanas/GestorVentana.java
+++ Proyecto_ProgramacionIV/src/bomberman/ventanas/GestorVentana.java
@@ -8,85 +8,86 @@ import bomberman.jugador.Jugador;
  * Clase que se encarga de gestionar las ventanas del programa. Muy útil y
  * además ayuda a que el código y el manejo del programa sea más claro y
  * eficiente.
- * 
+ *
  * @author David
  * @version 1.0
  */
 public class GestorVentana {
 
-	private static final long serialVersionUID = -7461719037402108362L;
-	private static Vector<Window> vectorVentanas = iniciar();
+        private static final long serialVersionUID = -7461719037402108362L;
+        private static Vector<Window> vectorVentanas = iniciar();
 
-	/**
-	 * Inicializamos el Vector y le añadimos todas las ventanas, las cuales
-	 * serán las que se mostrarán en el programa.
-	 * 
-	 * @param jug
-	 *            - Jugador
-	 */
-	public static Vector<Window> iniciar() {
-		// Inicializamos el vector
-		Jugador jug = new Jugador("david", "h", "h", "h");
-		Vector<Window> vector = new Vector<Window>();
-		// Se incializan todas las ventanas.
-		vector.add(new VentanaInicial());
-		vector.add(new VentanaDatos(jug));
-		vector.add(new VentanaSeleccion());
-		vector.add(new VentanaJuego());
-		// Se devuelve el vector con las ventanas
-		return vector;
-	}
+        /**
+         * Inicializamos el Vector y le añadimos todas las ventanas, las cuales
+         * serán las que se mostrarán en el programa.
+         *
+         * @param jug
+         *            - Jugador
+         */
+        public static Vector<Window> iniciar() {
+                // Inicializamos el vector
+                Jugador jug = new Jugador("david", "h", "h", "h");
+                Vector<Window> vector = new Vector<Window>();
+                // Se incializan todas las ventanas.
+                vector.add(new VentanaInicial());
+                vector.add(new VentanaDatos(jug));
+                vector.add(new VentanaSeleccion());
+                vector.add(new VentanaJuego());
+                // Se devuelve el vector con las ventanas
+                return vector;
+        }
 
-	/**
-	 * Método que se encarga de hacer visible la ventana que se indique por los
-	 * parámetros de entrada.
-	 * 
-	 * @param claseVentana
-	 *            - Class
-	 * @param unicaVisible
-	 *            - boolean, indica si es la única ventana que se deja visible o
-	 *            no. true si es la única, false si no.
-	 */
-	public static void hacerVisible(Class claseVentana, boolean unicaVisible) {
-		for (Window vent : vectorVentanas) {
-			// En caso de que la ventana sea de la clase que queremos.
-			if (vent.getClass().isAssignableFrom(claseVentana)) {
-				vent.setVisible(true);
-			} else {
-				/*
-				 * Si queremos que la ventana sea la única visible debemos
-				 * ocultar las demas.
-				 */
-				if (unicaVisible) {
-					vent.setVisible(false);
-				}
-			}
-		}
-	}
+        /**
+         * Método que se encarga de hacer visible la ventana que se indique por los
+         * parámetros de entrada.
+         *
+         * @param claseVentana
+         *            - Class
+         * @param unicaVisible
+         *            - boolean, indica si es la única ventana que se deja visible o
+         *            no. true si es la única, false si no.
+         */
+        public static void hacerVisible(Class claseVentana, boolean unicaVisible) {
+                for (Window vent : vectorVentanas) {
+                        // En caso de que la ventana sea de la clase que queremos.
+                        if (vent.getClass().isAssignableFrom(claseVentana)) {
+                                vent.setVisible(true);
+                        } else {
+                                /*
+                                 * Si queremos que la ventana sea la única visible debemos
+                                 * ocultar las demas.
+                                 */
+                                if (unicaVisible) {
+                                        vent.setVisible(false);
+                                }
+                        }
+                }
+        }
 
-	/**
-	 * Método que se encarga de ocultar la ventana que sea de la misma clase que
-	 * se indica en el parámetro de entrada.
-	 * 
-	 * @param claseVentana
-	 *            - Class
-	 */
-	public static void ocultarVentana(Class claseVentana) {
-		for (Window vent : vectorVentanas) {
-			if (vent.getClass().isAssignableFrom(claseVentana)) {
-				vent.setVisible(false);
-				// Cortamos el for.
-				break;
-			}
-		}
-	}
+        /**
+         * Método que se encarga de ocultar la ventana que sea de la misma clase que
+         * se indica en el parámetro de entrada.
+         *
+         * @param claseVentana
+         *            - Class
+         */
+        public static void ocultarVentana(Class claseVentana) {
+                for (Window vent : vectorVentanas) {
+                        if (vent.getClass().isAssignableFrom(claseVentana)) {
+                                vent.setVisible(false);
+                                // Cortamos el for.
+                                break;
+                        }
+                }
+        }
 
-	public static Object getVentana(Class claseVentana) {
-		for (Window vent : vectorVentanas) {
-			if (vent.getClass().isAssignableFrom(claseVentana)) {
-				return vent;
-			}
-		}
-		return null;
-	}
+        public static Object getVentana(Class claseVentana) {
+                for (Window vent : vectorVentanas) {
+                        if (vent.getClass().isAssignableFrom(claseVentana)) {
+                                return vent;
+                        }
+                }
+                return null;
+        }
 }
+
