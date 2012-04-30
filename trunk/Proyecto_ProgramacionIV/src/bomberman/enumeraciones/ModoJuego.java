77bba2264583c6006d23e77a12e4900c6dc96b27
david.orive@opendeusto.es <david.orive@opendeusto.es@9aa124b0-7f85-95f2-32f4-e1b9b7fe5357>
2012-04-30 08:15:14 +0000

    Comentado todo el paquete 'bomberman.enumeraciones'.

--- trunk/Proyecto_ProgramacionIV/src/bomberman/enumeraciones/ModoJuego.java
+++ trunk/Proyecto_ProgramacionIV/src/bomberman/enumeraciones/ModoJuego.java
@@ -1,5 +1,10 @@
 package bomberman.enumeraciones;
 
+/**
+ * Enumeración de tipos de modo de juego.
+ * @author David
+ * @version 1.0
+ */
 public enum ModoJuego {
 	Historia, Multijugador, Master
 }
--- trunk/Proyecto_ProgramacionIV/src/bomberman/enumeraciones/TiposLlama.java
+++ trunk/Proyecto_ProgramacionIV/src/bomberman/enumeraciones/TiposLlama.java
@@ -1,5 +1,9 @@
 package bomberman.enumeraciones;
-
+/**
+ * Enumeración de tipos de llama.
+ * @author David
+ * @version 1.0
+ */
 public enum TiposLlama {
 	CENTRAL, INTER_VER, INTER_HOR, PUNTA_DER, PUNTA_IZQ, PUNTA_ARRIBA, PUNTA_ABJ
 }
